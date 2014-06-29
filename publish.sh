c8c5beffee9440140930fca713b9b45a1e85deff
dwkns <dwkns@me.com>
2014-06-29 23:41:46 +0100

    re-fector (publish v1.38)

--- publish.sh
+++ publish.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
 # Auto incriments the version number of Krep and then uploads it to git.
-
 # get the last git commit message
-gitCommitMessage=`git log -1 --pretty=%B`;
+fullGitCommitMessage=`git log -1 --pretty=%B`;
+
+echo $fullGitCommitMessage
+gitCommitMessage=`echo ${fullGitCommitMessage%%(publish*}` # Result: aaa
+echo $gitCommitMessage
+
+# # get the directory that this script is running from.
+# DIR="$( cd "$( dirname "$0" )" && pwd )";
 
 
-# get the directory that this script is running from.
-DIR="$( cd "$( dirname "$0" )" && pwd )";
+# # read the current version number from the app plist
+# CFBundleShortVersionString=`defaults read $DIR/Krep.app/Contents/Info.plist CFBundleShortVersionString`;
+# CFBundleVersion=`defaults read $DIR//Krep.app/Contents/Info.plist CFBundleVersion`;
 
+# # increase the version numbers by 0.1
+# newCFBundleShortVersionString=`echo $CFBundleShortVersionString + 0.01 | bc`;
+# newCFBundleVersion=`echo $CFBundleVersion + 0.01 | bc`;
 
-# read the current version number from the app plist
-CFBundleShortVersionString=`defaults read $DIR/Krep.app/Contents/Info.plist CFBundleShortVersionString`;
-CFBundleVersion=`defaults read $DIR//Krep.app/Contents/Info.plist CFBundleVersion`;
-
-# increase the version numbers by 0.1
-newCFBundleShortVersionString=`echo $CFBundleShortVersionString + 0.01 | bc`;
-newCFBundleVersion=`echo $CFBundleVersion + 0.01 | bc`;
-
-# write the new version numbers to the plists
-defaults write $DIR/Krep.app/Contents/Info.plist CFBundleShortVersionString $newCFBundleShortVersionString;
-defaults write $DIR/Krep.app/Contents/Info.plist CFBundleVersion $newCFBundleVersion;
+# # write the new version numbers to the plists
+# defaults write $DIR/Krep.app/Contents/Info.plist CFBundleShortVersionString $newCFBundleShortVersionString;
+# defaults write $DIR/Krep.app/Contents/Info.plist CFBundleVersion $newCFBundleVersion;
 
 
 # do the git commit
-git add -A;
-git commit -m "$gitCommitMessage (publish)";
-git push --all;
+# git add -A;
+# git commit -m \"$gitCommitMessage (publish)\";
+# git push --all;
