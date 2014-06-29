4646ef2062be44ab9d4487a5a5aa6ab4fc632525
dwkns <dwkns@me.com>
2014-06-29 23:23:12 +0100

    re-fector (publish) (publish)

--- publish.sh
+++ publish.sh
@@ -1,44 +1,29 @@
 #!/bin/bash
-# defaults read /Applications/Krep.app/Contents/Info.plist 
-# defaults write /Applications/Krep.app/Contents/Info.plist  CFBundleShortVersionString 1.3
 
-gitCommitMessage=`git log -1 --pretty=%B`
-echo $gitCommitMessage
+# Auto incriments the version number of Krep and then uploads it to git.
 
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-echo $DIR
+# get the last git commit message
+gitCommitMessage=`git log -1 --pretty=%B`;
 
 
-CFBundleShortVersionString=`defaults read $DIR/Krep.app/Contents/Info.plist CFBundleShortVersionString`
-CFBundleVersion=`defaults read $DIR//Krep.app/Contents/Info.plist CFBundleVersion`
-
-echo $CFBundleShortVersionString
-
-newCFBundleShortVersionString=`echo $CFBundleShortVersionString + 0.01 | bc`
-newCFBundleVersion=`echo $CFBundleVersion + 0.01 | bc`
+# get the directory that this script is running from.
+DIR="$( cd "$( dirname "$0" )" && pwd )";
 
 
-defaults write $DIR/Krep.app/Contents/Info.plist CFBundleShortVersionString $newCFBundleShortVersionString
-defaults write $DIR/Krep.app/Contents/Info.plist CFBundleVersion $newCFBundleVersion
+# read the current version number from the app plist
+CFBundleShortVersionString=`defaults read $DIR/Krep.app/Contents/Info.plist CFBundleShortVersionString`;
+CFBundleVersion=`defaults read $DIR//Krep.app/Contents/Info.plist CFBundleVersion`;
 
-CFBundleShortVersionString=`defaults read $DIR/Krep.app/Contents/Info.plist CFBundleShortVersionString`
-echo $CFBundleShortVersionString
+# increase the version numbers by 0.1
+newCFBundleShortVersionString=`echo $CFBundleShortVersionString + 0.01 | bc`;
+newCFBundleVersion=`echo $CFBundleVersion + 0.01 | bc`;
 
-git add -A
-git commit -m "$gitCommitMessage (publish)"
-git push --all
+# write the new version numbers to the plists
+defaults write $DIR/Krep.app/Contents/Info.plist CFBundleShortVersionString $newCFBundleShortVersionString;
+defaults write $DIR/Krep.app/Contents/Info.plist CFBundleVersion $newCFBundleVersion;
 
 
-
-# alias gc='git commit'
-# alias ga='git add -A'
-# alias gs='git status'
-# alias gb='git branch'
-# alias gp='git push --all'
-# alias gco='git checkout'
-
-# num=$(($num1 + $num2))
-# echo $num
-
-# echo CFBundleVersion
-# git log -1 --pretty=%B
+# do the git commit
+git add -A;
+git commit -m "$gitCommitMessage (publish)";
+git push --all;
