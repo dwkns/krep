da02b5dbd2ec1f9ef9e705af58c45a7c9707a177
dwkns <dwkns@me.com>
2015-02-12 21:22:10 +0000

    Volume adjustment (publish v)

--- publish.sh
+++ publish.sh
@@ -7,7 +7,8 @@ gitCommitMessage=`echo ${fullGitCommitMessage%%(publish*}`;
 
 # read the current version number from the app plist;
 CFBundleShortVersionString=`defaults read "$currentDir/Krep.app/Contents/Info.plist" CFBundleShortVersionString`;
-newBundleVersion=`echo $CFBundleShortVersionString + 0.01 | bc`;
+echo "Current Version is CFBundleShortVersionString"
+$newBundleVersion=`echo $CFBundleShortVersionString + 0.01 | bc`;
 
 # write the new version numbers to the plists;
 defaults write "$currentDir/Krep.app/Contents/Info.plist" CFBundleShortVersionString $newBundleVersion;
